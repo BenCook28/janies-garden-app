16b012cecbf3c5007cf2de4bd48177e640fd14f2
Ben Cook <benthomascook28@gmail.com>
2017-08-03 13:11:08 -0400

    Added Nav Bar and Signin Template

---
+++ janies-garden-app/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+        path: 'signin',
+        component: SigninComponent
+    },
+    {
+      path: 'signup',
+      component: SignupComponent
+    }
+  ])  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
