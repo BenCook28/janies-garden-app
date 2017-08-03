43eb9d46d6628a9c9c715e4f18cc1eb36f340362
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-08-03 13:40:43 -0400

    first commit on develop branch

---
+++ janies-garden-app/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { JaniesGardenAppPage } from './app.po';
+
+describe('janies-garden-app App', () => {
+  let page: JaniesGardenAppPage;
+
+  beforeEach(() => {
+    page = new JaniesGardenAppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
