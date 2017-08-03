ccd69bcbc5f0030978b7d0de8242e1591017a3ac
Ben Cook <benthomascook28@gmail.com>
2017-08-03 14:39:10 -0400

    Removed OnInit from signin.ts

--- janies-garden-app/src/app/signin/signin.component.ts
+++ janies-garden-app/src/app/signin/signin.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent {
 
   constructor(
   	public email: string,
