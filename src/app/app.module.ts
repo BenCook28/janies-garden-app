3f31d5fda958d7293c8965cad0d4d2a18d6c02cb
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-08-07 15:39:36 -0400

    Add Plant view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { AddPlantComponent } from './addplant/addplant.component';
+import { ImageContainerComponent } from './addplant/image-container/image-container.component';
+import { FileUploaderModule } from '@uniprank/ngx-file-uploader';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    AddPlantComponent,
+    ImageContainerComponent/*,*/
+    // FileUploaderModule
   ],
   imports: [
+    FileUploaderModule,
     BrowserModule,
     RouterModule.forRoot([
       {
@@ -24,6 +31,10 @@ import { SignupComponent } from './signup/signup.component';
       {
         path: 'signup',
         component: SignupComponent
+      },
+      {
+        path: 'addplant',
+        component: AddPlantComponent
       }
     ])
   ],
