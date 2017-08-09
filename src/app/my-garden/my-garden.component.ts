31ecafabfda709eefdb6e73c269a90595e0b9f31
Ben Cook <benthomascook28@gmail.com>
2017-08-09 16:16:03 -0400

    Removed Errors in components and services

--- src/app/my-garden/my-garden.component.ts
+++ src/app/my-garden/my-garden.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantsService } from '../services/plants.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Plant } from '../models/plants'
 
 @Component({
   selector: 'my-garden',
@@ -6,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-garden.component.css']
 })
 export class MyGardenComponent implements OnInit {
+  plant = this.plant;
 
-  constructor() { }
+  onClick(){
+			this.router.navigateByUrl('/addplant');
+        }
+  constructor(private route: ActivatedRoute, private router: Router, private ps: PlantsService) {
+    // this.route.params.subscribe((params) {
+    //     this.ps.getUserInfo(params.id).then((prof) => {
+    //     this.modelProfile = new Profile(prof.username, prof.name, prof.gender, prof.about, prof.hobbies, prof.age, prof.langs, prof.prefgender);
+    //     this.hobbies = prof.hobbies;
+    //     this.langs = prof.langs;
+    //   })
+    // })
+   }
 
   ngOnInit() {
   }
--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { Router } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
@@ -10,5 +12,15 @@ export class PlantsService {
 	createPlant(plant: Plant) {
 		this.afd.database.ref("plants/" + plant.common).set(plant);
 	}
-	constructor(private afd: AngularFireDatabase, private router: Router){}
+	constructor(private afd: AngularFireDatabase, private af: AngularFireAuth, private router: Router){}
+
+	onSubmit(user: User){
+		this.af.auth.signInWithEmailAndPassword(user.email, user.pass)
+		.then(() => {
+			this.router.navigateByUrl('/my-garden');
+		})
+		.catch((e) => {
+			console.log(e);
+		})
+	}
 }
