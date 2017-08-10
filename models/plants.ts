572e0e3392b53e631146de0fea550f1683e9b4d8
Ben Cook <benthomascook28@gmail.com>
2017-08-10 16:35:19 -0400

    Recovering Files from Git Init

---
+++ models/plants.ts
@@ -0,0 +1,17 @@
+export class Plant {
+	constructor(
+		public common: string,
+		public scientific: string,
+		public imgurl: string,
+		public water: string,
+		public sun: string,
+		public planted: string,
+		public bloomed: string,
+		public location: string,
+		public height: string,
+		public width: string,
+		public native: string
+
+	) {}
+	
+}
