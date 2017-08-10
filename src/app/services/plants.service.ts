784f3766ac682459abe57df641507e32b0f68838
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-08-10 16:01:54 -0400

    Corrected last push

--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -9,74 +9,61 @@ import { Plant } from '../models/plants';
 
 @Injectable()
 export class PlantsService {
-	createPlant(plant: Plant) {
-		// this.afd.database.ref("plants/" + plant.common).set(plant)
-		this.afd.database.ref("plants/").push(plant)
-		.then(() => {
-			this.router.navigateByUrl('/my-garden');
-		})
-		//.then
-			//navigate to the url
-		//.catch
-			//print out the error
-	}
+    createPlant(plant: Plant) {
+        // this.afd.database.ref("plants/" + plant.common).set(plant)
+        this.afd.database.ref("plants/").push(plant)
+        .then(() => {
+            this.router.navigateByUrl('/my-garden');
+        })
+        //.then
+            //navigate to the url
+        //.catch
+            //print out the error
+    }
 
-<<<<<<< HEAD
 updatePlant(plant: Plant) {
-		this.afd.database.ref("plants/" + plant.common).update(plant);
-		//.then
-			//navigate to the url
-		//.catch
-			//print out the error
-	}
-	
+        this.afd.database.ref("plants/" + plant.common).update(plant);
+        //.then
+            //navigate to the url
+        //.catch
+            //print out the error
+    }
+    
 
-=======
->>>>>>> update
-	onSubmit(user: User){
-		this.af.auth.signInWithEmailAndPassword(user.email, user.pass)
-		.then(() => {
-			this.router.navigateByUrl('/my-garden');
-		})
-		.catch((e) => {
-			console.log(e);
-		})
-	}
+    onSubmit(user: User){
+        this.af.auth.signInWithEmailAndPassword(user.email, user.pass)
+        .then(() => {
+            this.router.navigateByUrl('/my-garden');
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+    }
 
-	
-	// getPlantInfo(email: string){
-	// 	//Returns promise with the snapshot of the user data
-	// 	this.afd.database.ref('user/' + email).once('value');
-	
-	// 		.then(function(s) {
-	// 		return s.val();
-	// 	});
-	getPlantInfo(){
-		return this.afd.database.ref('plants/').once('value')
-		.then(function(s) {
-			return s.val();
-		})
-	}
+    
+    // getPlantInfo(email: string){
+    //     //Returns promise with the snapshot of the user data
+    //     this.afd.database.ref('user/' + email).once('value');
+    
+    //         .then(function(s) {
+    //         return s.val();
+    //     });
+    getPlantInfo(){
+        return this.afd.database.ref('plants/').once('value')
+        .then(function(s) {
+            return s.val();
+        })
+    }
 
-<<<<<<< HEAD
-	getPlant(s:String){
-			this.afd.object('/plant/'+s)
-	}
-	// updatePlant(p:Plant){
-	// 		this.afd.object('/plant/'+p.common).update(p);
-	// }
-=======
-	updatePlant(plant: Plant) {
-		this.afd.database.ref("addplant/" + plant.common).update(plant);
-		//.then
-			//navigate to the url
-		//.catch
-			//print out the error
-	}
->>>>>>> update
+    getPlant(s:String){
+            this.afd.object('/plant/'+s)
+    }
+    // updatePlant(p:Plant){
+    //         this.afd.object('/plant/'+p.common).update(p);
+    // }
 
-	//Each plant by individual user id.
-	//each iitem to populate
-	constructor(private afd: AngularFireDatabase, private af: AngularFireAuth, private router: Router){}
+    //Each plant by individual user id.
+    //each iitem to populate
+    constructor(private afd: AngularFireDatabase, private af: AngularFireAuth, private router: Router){}
 
 }
