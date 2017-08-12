199493ee1d43540018361bc9bfc782c7f1675603
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-08-12 13:51:59 -0400

    Commented out some testing code

--- src/app/e2e/app.e2e-spec.ts
+++ src/app/e2e/app.e2e-spec.ts
@@ -1,15 +1,15 @@
-import {} from 'jasmine';
-import { JaniesGardenAppPage } from './app.po';
+// import {} from 'jasmine';
+// import { JaniesGardenAppPage } from './app.po';
 
-describe('janies-garden-app App', () => {
-  let page: JaniesGardenAppPage;
+// describe('janies-garden-app App', () => {
+//   let page: JaniesGardenAppPage;
 
-  beforeEach(() => {
-    page = new JaniesGardenAppPage();
-  });
+//   beforeEach(() => {
+//     page = new JaniesGardenAppPage();
+//   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
-});
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('Welcome to app!');
+//   });
+// });
